9dfdaef6dd41738a4aa44d75934ab289622e2738
unknown <dlckdals1004@naver.com>
2022-11-18 15:00:06 +0900

    reviews 1차 작업
    server의 reviews url, views, serializers 1차 작성(Vue 작성이후
    실행시키며 수정예정)

--- server/movies/urls.py
+++ server/movies/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # path('movies/<str:movies_filter>/',views.movies),
+    # path('movies/<int:movie_pk>/',views.movies_detail),
+    # path('crews/actor/',views.crews),
+    # path('crews/director',views.director),
 ]
