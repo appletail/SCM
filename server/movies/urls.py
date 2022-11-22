36e9430fbee3882a938c22ac2653474453a23c11
appletail <csappletail@gmail.com>
2022-11-22 10:31:44 +0900

    movies 백엔드 작성
    movies serializer 작성, urls 작성, views 작성

--- server/movies/urls.py
+++ server/movies/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.movies_list),
     path('popular/',views.movies_list_popular),
-    path('personal/',views.movies_list_personal),
     path('latest/',views.movies_list_latest),
-    # path('upcoming/',views.movies_list_upcoming),
+    path('genre/',views.movies_list_genre),
+    path('genre/<int:genre_pk>/',views.genre_detail),
+    path('personal/',views.movies_list_personal),
     path('<int:movie_pk>/',views.movies_detail),
-    path('crews/actor/',views.crews_list),
-    path('crews/director/',views.director_list),
+    path('crews/actor/<int:page>/',views.crews_list),
+    path('crews/director/<int:page>/',views.director_list),
     path('crews/<int:crew_pk>/',views.crews_detail),
-    path('makeDB/',views.makedb),
+    # path('makeDB/',views.makedb),
 ]
